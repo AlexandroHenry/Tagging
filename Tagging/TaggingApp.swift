f89f31e9b4a38376f7f15ef9ae11152941daf3bc
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-02-28 01:52:11 +0900

    Initial Commit

---
+++ Tagging/TaggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaggingApp.swift
+//  Tagging
+//
+//  Created by Seungchul Ha on 2023/02/28.
+//
+
+import SwiftUI
+
+@main
+struct TaggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
